f3e41c08734ef25392bbcc2a560fa3efac8400db
Jakub Hampl <kopomir@gmail.com>
2024-03-23 07:38:09 +0000

    Adds better tests

---
+++ e2e/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+cat << EOF > $SCRIPT_DIR/../tests/StandardModule.elm
+module StandardModule exposing (..)
+
+-- Do not edit this module directly, it is generated from ../e2e/Standard.elm
+
+standard : String
+standard =
+    """$(cat $SCRIPT_DIR/Standard.elm)"""
+EOF
+
+
+cp $SCRIPT_DIR/E2E.elm $SCRIPT_DIR/../tests/E2E.elm 
+cp $SCRIPT_DIR/Standard.elm $SCRIPT_DIR/../tests/Standard.elm 
+elm-review --config ./preview --fix-all-without-prompt $SCRIPT_DIR/../tests
+
+elm-test $SCRIPT_DIR/../tests/E2E.elm
+
+rm -f $SCRIPT_DIR/../tests/E2E.elm  $SCRIPT_DIR/../tests/Standard.elm 
