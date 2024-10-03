807dfd73f30c1ef7331546c0ffc43fc7aee6137d
Jakub Hampl <kopomir@gmail.com>
2024-10-03 16:30:38 +0100

    Renames rule and adds better error messages (#15)

--- preview/src/ReviewConfig.elm
+++ preview/src/ReviewConfig.elm
@@ -11,11 +11,11 @@ when inside the directory containing this file.
 
 -}
 
-import NoDebug.TodoItForMe
+import Derive
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    [ NoDebug.TodoItForMe.rule True []
+    [ Derive.rule True []
     ]
